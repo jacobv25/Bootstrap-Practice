de40f7e7cb53e13cdbce60ebe2e1635ef043fa2b
Jacob Valenzuela <valen306@cougars.csusm.edu>
2022-03-26 18:19:39 -0700

    Initial commit

---
+++ src/main/java/com/example/bootstrappractice/BootstrapPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstrappractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootstrappractice/BootstrapPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstrappractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
